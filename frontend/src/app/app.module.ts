b3c15de52746738ec85bd55324cff998c27d2514
NewKasim <123303268+NewKasim@users.noreply.github.com>
2023-05-18 13:16:16 +0530

    add new commonent propery-navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyNavbarComponent } from './property-navbar/property-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyNavbarComponent
   ],
   imports: [
     BrowserModule
