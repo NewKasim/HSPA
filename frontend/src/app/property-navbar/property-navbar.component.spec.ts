293e68139a2af476c450a329879127e2170c5c61
NewKasim <123303268+NewKasim@users.noreply.github.com>
2023-05-18 13:18:27 +0530

    new feature nav-bar add

---
+++ frontend/src/app/property-navbar/property-navbar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PropertyNavbarComponent } from './property-navbar.component';
+
+describe('PropertyNavbarComponent', () => {
+  let component: PropertyNavbarComponent;
+  let fixture: ComponentFixture<PropertyNavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PropertyNavbarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PropertyNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/property-navbar/property-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-navbar',
+  templateUrl: './property-navbar.component.html',
+  styleUrls: ['./property-navbar.component.css']
+})
+export class PropertyNavbarComponent {
+
+}
